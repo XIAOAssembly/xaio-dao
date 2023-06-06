b88f1a7a6262962052c6a3b1ca73bc47a0bba8b7
patridactyl <channel.42@icloud.com>
2023-06-06 11:31:03 -0700

    add SEO head

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – XAIO DAO',
+      }
+    }
+  }, 
+  head:() => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://xaio-dao.vercel.app/' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Nextra'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'The next site builder'}
+        />
+      </>
+    )
+  },  
+  primaryHue: 180,
   logo: <span>XAIO DAO</span>,
   project: {
     link: 'https://github.com/XIAOAssembly/xaio-dao',
@@ -9,7 +37,7 @@ const config: DocsThemeConfig = {
   chat: {
     link: 'https://discord.gg/HgMH9xJM',
   },
-  docsRepositoryBase: 'https://github.com/XIAOAssembly/xaio-dao',
+  docsRepositoryBase: 'https://github.com/XIAOAssembly/xaio-dao/tree/main/',
   footer: {
     text: 'XAIO DAO',
   },
